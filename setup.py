1002828ad717e8f71d2b6f8294d09c4a3c066503
Stan Seibert <stan@mtrr.org>
2011-09-29 11:00:34 -0400

    Add a setup.py script so egret can be installed into a virtualenv using pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='egret',
+    version='1.0',
+    description='A really minimal couchdb application building thingy.',
+    author='Andy Mastbaum',
+    author_email='amastbaum@gmail.com',
+    scripts=['egret'],
+)
